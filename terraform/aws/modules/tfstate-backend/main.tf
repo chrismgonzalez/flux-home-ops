2a031ab5caa909db5f1e7ddbaa3e00dae56abfe2
Chris Gonzalez <cmgonza98@gmail.com>
2023-01-04 11:29:01 -0500

    update backend details in tf

---
+++ terraform/aws/modules/tfstate-backend/main.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "backend" {
+  bucket = "${var.bucket_prefix}-terraform-backend"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.bucket_sse_algorithm
+    }
+  }
+}
+resource "aws_s3_bucket_public_access_block" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "lock" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
